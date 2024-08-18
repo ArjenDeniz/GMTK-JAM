2274db186a79123331e44a15c36e819e32d87515
moneylobster <87999452+moneylobster@users.noreply.github.com>
2024-08-19 00:24:10 +0300

    Make things themable, overhaul future theme

--- resource_slot.gd
+++ resource_slot.gd
@@ -1,10 +1,4 @@
-extends Control
-
-@onready var details = $Details
-@onready var details_text = $Details/Label
-@onready var Quantity = $Innter/Quantity
-@onready var Icon = $Innter/ItemIcon
-
+extends Button
 
 var resource = {"name": 'Default Resource', "quantity": -1, "texture": "res://Icons/science.png"}
 # Called when the node enters the scene tree for the first time.
@@ -28,17 +22,6 @@ func load_json_data(file_path):
 			print('Parse error in ',file_path)
 	else:
 		print('Couldn\'t find resource data ')
-		
-func _on_button_mouse_entered() -> void:
-	details.show()
-	details_text.show()
-	return
-
-
-func _on_button_mouse_exited() -> void:
-	details.hide()
-	details_text.hide()
-	return
 
 func add_resource(new_resource):
 	resource['name'] = new_resource['name']
@@ -49,15 +32,14 @@ func add_resource(new_resource):
 		resource['texture'] = texture_data[resource['name'] ]
 	else:
 		resource['texture'] = "res://icon.svg"
-	$Innter/ItemIcon.texture = load(resource['texture'])
+	icon = load(resource['texture'])
 	
-	#Icon.texture = ResourceLoader.load(resource['texture'])
-	$Innter/Quantity.text= str(resource['quantity'])
-	$Details/Label.text = str(resource['name'])
+	text = str(resource['quantity'])
+	tooltip_text = str(resource['name'])
 	
 	
 func update_quantity(new_quantity):
 	resource['quantity'] =new_quantity
-	$Innter/Quantity.text = str(resource['quantity'])
+	text = str(resource['quantity'])
 	return
 	
